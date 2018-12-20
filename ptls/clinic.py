5a4231f8a283098edde5162664657939c1456145
Roshan Giyanani <roshangiyanani@gmail.com>
2018-12-20 12:54:31 -0500

    Improve Clinic's typing and add support for writing to a csv.

--- ptls/clinic.py
+++ ptls/clinic.py
@@ -1,15 +1,19 @@
+import csv
+from pathlib import Path
+
 
 class Clinic:
     company: str
     name: str
     address: str
     phone: str
-    fax: str
-    email: str
+    fax: str or None
+    email: str or None
     url: str
 
-    def __init__(self, company: str, name: str, address: str, phone: str, url: str, fax: str = None, email: str = None):
-        self.company = company
+    def __init__(self, company: str, name: str, address: str, phone: str,
+                 url: str, fax: str or None = None, email: str or None = None):
+        self.company = company;
         self.name = name
         self.address = address
         self.phone = phone
@@ -17,14 +21,22 @@ class Clinic:
         self.fax = fax
         self.email = email
 
+    def as_row(self) -> [str]:
+        return [self.company or '', self.name or '', self.address or '',
+                self.url or '', self.phone or '', self.email or '', self.fax or '']
+
+    @staticmethod
+    def get_header() -> [str]:
+        return ['company', 'name', 'address', 'url', 'phone', 'email', 'fax']
+
     def __eq__(self, other):
-        return self.company == other.company\
-            and self.name == other.name\
-            and self.address == other.address\
-            and self.phone == other.phone\
-            and self.url == other.url\
-            and self.fax == other.fax\
-            and self.email == other.email\
+        return self.company == other.company \
+               and self.name == other.name \
+               and self.address == other.address \
+               and self.phone == other.phone \
+               and self.url == other.url \
+               and self.fax == other.fax \
+               and self.email == other.email
 
     def __ne__(self, other):
         return not self.__eq__(other)
@@ -39,3 +51,11 @@ class Clinic:
             f'fax: {self.fax}',
             f'email: {self.email}',
         ])
+
+
+def write_clinic_list(clinics: [Clinic], file: Path):
+    with file.with_suffix('.csv').open('w', newline='') as csvfile:
+        writer = csv.writer(csvfile, 'excel')
+        writer.writerow(Clinic.get_header())
+        for clinic in clinics:
+            writer.writerow(clinic.as_row())
