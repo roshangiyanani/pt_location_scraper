7db500d15afe7de0f9ed7050378379c93dbc4b13
Roshan Giyanani <roshangiyanani@gmail.com>
2018-12-30 15:06:19 -0500

    Update requester to do user agent spoofing (for pivot website).

--- ptls/requester.py
+++ ptls/requester.py
@@ -1,7 +1,15 @@
 from requests import get, Response
 from requests.exceptions import RequestException
+from requests.utils import default_headers
 from bs4 import BeautifulSoup
 import time
+
+headers = default_headers()
+headers.update(
+    {
+        'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_14_2) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/12.0.2 Safari/605.1.15',
+    }
+)
 
 class Requester:
     """
@@ -64,7 +72,7 @@ class Requester:
             :return: raw string of contents
             """
         try:
-            with get(url, stream=True) as resp:
+            with get(url, stream=True, headers=headers) as resp:
                 if Requester.is_good_response(resp):
                     raw_html: str = resp.content
                     return raw_html
